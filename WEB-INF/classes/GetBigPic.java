55646f21a98f7ff7bed0d0df10b4697f454c4075
tkevin <tkevin@ualberta.ca>
2014-11-21 00:27:49 -0700

    Initial update image

--- WEB-INF/classes/GetBigPic.java
+++ WEB-INF/classes/GetBigPic.java
@@ -2,6 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *  This servlet sends one picture stored in the table below to the client 
@@ -32,6 +33,8 @@ public class GetBigPic extends HttpServlet
 	throws ServletException, IOException {
 	
 	//  construct the query  from the client's QueryString
+		HttpSession session = request.getSession();
+	String userName=(String)session.getAttribute("USERNAME");
 	String picid  = request.getQueryString();
 	String query;
 
@@ -50,8 +53,8 @@ public class GetBigPic extends HttpServlet
 	    Statement stmt = conn.createStatement();
 	    ResultSet rset = stmt.executeQuery(query);
 	    response.setContentType("text/html");
-            String subject, place, owner_name, timing, description;
-
+        String subject, place,timing, description;
+		String owner_name = null;
 	    if ( rset.next() ) {
 	        subject = rset.getString("subject");
 	        place = rset.getString("place");
@@ -62,11 +65,39 @@ public class GetBigPic extends HttpServlet
 	                 "<body bgcolor=\"#000000\" text=\"#cccccc\">" +
 		 "<center><img src = \"/proj1/GetOnePic?"+picid+"\">" +
 			 "<h3>" + subject +"  at " + place + " </h3>" + "<br> taken by: " + owner_name + " on "+timing +
-			 "<br> <br>" + description +
-			 "</body></html>");
+			 "<br> <br>" + description);
             }
 	    else
 	      out.println("<html> Pictures are not avialable</html>");
+		  
+		if (userName == owner_name){
+			out.println("HELLO </BODY></HTML>");	
+		}
+		else{ 
+ 		   ArrayList<String> privacy=new ArrayList<String>();
+		   privacy.add("public");
+		   privacy.add("private");  
+		    ResultSet rset2 = null;
+	    	String sql1 = "select group_name, user_name from groups where user_name ='"+userName+"'";
+			rset2 = stmt.executeQuery(sql1);
+			String nameCreator="";
+			String groupName = "";
+			String groupCreator="";
+			while(rset2 != null && rset.next()){
+				groupName = (rset.getString(1)).trim();
+				groupCreator=(rset.getString(2)).trim();
+				nameCreator=groupName+","+groupCreator;
+				privacy.add(nameCreator);
+				}
+		out.println("</center>" +"<h4> Edit Image: </h4>"
+			+ "<form action='editImage' method='post'>"
+			+ "Subject: " + "<input type='text' name='SUBJECT' maxlength='128' size='30'> <br>" 
+			+ "Place: "+"<input type='text' name='PLACE' maxlength='128' size='30'><br>"
+			+ "Description: "+"<textarea type='text' name='DESCRIPTION' maxlength='2048' rows='10' cols='25'></textarea> <br>"
+			
+			+ "<input type='submit' name='editButton' value='Update' />"
+			+ "</form>");
+		out.println("GOODBYE</body></html>"); }
 	} catch( Exception ex ) {
 	    out.println(ex.getMessage() );
 	}
@@ -79,10 +110,11 @@ public class GetBigPic extends HttpServlet
 	    }
 	}
     }
-
+	
 	    /*
      *   Connect to the specified database
      */
+	 
     private Connection getConnected() throws Exception {
 
 	String username = "elbohtim";
