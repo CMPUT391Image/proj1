d0e700b3efc601c6e1805d0e2614390553f119d3
telbohtimy <elbohtim@ualberta.ca>
2014-11-24 16:49:57 -0700

    update with groups (double check in case i missed something

--- WEB-INF/classes/GetBigPic.java
+++ WEB-INF/classes/GetBigPic.java
@@ -77,21 +77,30 @@ public class GetBigPic extends HttpServlet
 		}
 		else{ 
  		   ArrayList<String> privacy=new ArrayList<String>();
+                   privacy.add("none");
 		   privacy.add("public");
 		   privacy.add("private");  
-		    ResultSet rset2 = null;
-	    	String sql1 = "select group_name, user_name from groups where user_name ='"+userName+"'";
-			rset2 = stmt.executeQuery(sql1);
-			String nameCreator="";
-			String groupName = "";
-			String groupCreator="";
-			//getting permissions
-			while(rset2 != null && rset.next()){
-				groupName = (rset.getString(1)).trim();
-				groupCreator=(rset.getString(2)).trim();
-				nameCreator=groupName+","+groupCreator;
-				privacy.add(nameCreator);
-				}
+		   ResultSet rset2 = null;
+	    	   String sql1 = "select group_name, user_name from groups where user_name ='"+userName+"'";
+		   rset2 = stmt.executeQuery(sql1);
+		   String nameCreator="";
+		   String groupName = "";
+		   String groupCreator="";
+		   //getting permissions
+		   while(rset2 != null && rset2.next()){
+		       groupName = (rset2.getString(1)).trim();
+		       groupCreator=(rset2.getString(2)).trim();
+		       nameCreator=groupName+","+groupCreator;
+		       privacy.add(nameCreator);
+		   }
+		   String sql2 = "select g1.group_name, g1.user_name from groups g1,group_lists g2 where g1.group_id=g2.group_id and g2.friend_id = '"+userName+"'";
+                   rset2 = stmt.executeQuery(sql2);
+                   while(rset2 != null && rset2.next()){
+		       groupName = (rset2.getString(1)).trim();
+		       groupCreator=(rset2.getString(2)).trim();
+		       nameCreator=groupName+","+groupCreator;
+		       privacy.add(nameCreator);
+		   } 
 				//Creating form, onclick = doPost
 				out.println("</center>"
 						+ "<h4> Edit Image: </h4>"
@@ -103,10 +112,12 @@ public class GetBigPic extends HttpServlet
 						+ "Description: <Br> "
 						+ "<textarea type='text' name='new_description' maxlength='2048' rows='10' cols='25'></textarea> <br>"
 						+ "<th>Privacy (Group Name, Creator):</th>"
-						+ " <td><select name='new_privacy'>"
-						+ "<% for (String item:privacy){%>  "
-						+ "<option value='<%=item %>'>" + " <%=item%>"
-						+ " </option>" + " <%}" + " %>" +"</td>" + "</select>"
+					        + " <td><select name='new_privacy'>");
+				for (String item:privacy){//TAREK CHANGE
+				    out.println( "<option value='"+item+"'>"+item 
+						 + " </option>"); 
+				 }
+				out.println( "</select></td>"
 						+ "<input type='submit' name='editButton' value='update' />"
 						+ "</form>");
 				out.println("GOODBYE</body></html>"); 
@@ -133,6 +144,7 @@ public class GetBigPic extends HttpServlet
 	throws ServletException, IOException {
 		Connection conn = null;
 		Statement stmt = null;
+                ResultSet rset=null;//TAREK CHANGE
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
 		String userName=(String)session.getAttribute("USERNAME");
@@ -141,7 +153,18 @@ public class GetBigPic extends HttpServlet
 		String place = request.getParameter("new_place");
 		String description = request.getParameter("new_description");
 		String sql = "Update images set ";
-		if (subject != "" || place != "" || description != ""){
+                String privacy = request.getParameter("new_privacy");//TAREK CHANGE
+                try{
+			conn = getConnected();
+			stmt = conn.createStatement();
+			
+		 }
+			
+		catch(Exception ex){
+		    out.println("<hr>" + sql +"<hr>");
+		    out.println("<hr>" + ex.getMessage() + "<hr>");
+		}
+		if (subject != "" || place != "" || description != "" || !privacy.equals("none")){//TAREK CHANGE
 			if (subject != ""){
 				sql+= "subject = '" + subject+ "'";
 			}
@@ -157,8 +180,39 @@ public class GetBigPic extends HttpServlet
 				}
 				sql+= "description = '" + description+ "'";
 			}
+                        if (!privacy.equals("none")){
+                            String privacy_int="";
+			    if(privacy.equals("public")){
+				privacy_int="1";
+			    }
+			    else if(privacy.equals("private")){
+				privacy_int="2";
+			    }
+ 
+			    else{
+				String[] parts=privacy.split(",");
+				String name=parts[0];
+				String creator=parts[1];
+				try{
+				    String sql1="select group_id from groups where group_name='"+name+"' and user_name='"+creator+"'";
+				    rset=stmt.executeQuery(sql1);
+				    while(rset != null && rset.next())
+				    privacy_int=(rset.getString(1)).trim();
+				    sql+="permitted = '" + privacy_int+"'";
+				}
+				catch(Exception ex1){
+				    out.println("<hr>" + ex1.getMessage() + "<hr>");
+				}
+			    }
+			}
 			sql += " where photo_id = " + picid;
-			try{
+                        try{
+                            stmt.executeUpdate(sql);
+                        }
+                        catch(Exception e){
+			     out.println("<hr>" + e.getMessage() + "<hr>");
+			}
+			/*try{
 			conn = getConnected();
 			stmt = conn.createStatement();
 			stmt.executeUpdate(sql);
@@ -168,7 +222,7 @@ public class GetBigPic extends HttpServlet
 			catch(Exception ex){
 				out.println("<hr>" + sql +"<hr>");
 			   out.println("<hr>" + ex.getMessage() + "<hr>");
-			 }
+			   }*?*/
 		}
 		
 		doGet(request,response);
