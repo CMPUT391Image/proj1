6c9623c53653fa627b005bc73723ef0ac2b549f3
tkevin <tkevin@ualberta.ca>
2014-11-26 16:11:37 -0700

    OLAP DONE

--- WEB-INF/classes/Olap.java
+++ WEB-INF/classes/Olap.java
@@ -23,21 +23,20 @@ public class Olap extends HttpServlet {
 		out.println("<div style='float: right'>"
 				+ "<form name='logout' method='post' action='logout.jsp'>"
 				+ "<input type='submit' name='logout' value='logout'> </form>"
-                                +"<form name=help method=get action=menu.jsp><input type=submit name=menu value=menu></form>"
-                                +"<form name=help method=get action=help.html><input type=submit name=help value=help></form>"
 				+ "</div>");
 		out.println("<body> <H1>Data Analysis</H1>"
 				+ "<form name='olapresult' method='post' action = 'Olap'>"
-				+ "Analyze Options:"
-				+ "<input type='checkbox' name='imageowner' value='1'></input> User"
-				+ "<input type='checkbox' name='subjectBox' value='1'></input> Subject: <br>"
+				+ "Analyze Options:<br>"
+				+ "<input type='checkbox' name='imageowner' value='1'></input> User: "
+                            	+ "<input name='ownerText' type='text' size='30'></input> <br>"
+				+ "<input type='checkbox' name='subjectBox' value='1'></input> Subject: "
 				+ "<input name='subjectText' type='text' size='30'></input> <br>"
 				+ "<input type='checkbox' name='time' value='1'></input> Find by date: <br>"
 				+ "from: <input name='fromTime' placeholder ='DD/MM/YYYY' type='text' size='30'></input> <br>"
 				+ "to: <input name='toTime' placeholder ='DD/MM/YYYY'type='text' size='30'></input><br>"
 				+ "<hr>"
 				+ "Order Time Period by:"
-				+ "<input type='radio' name='order' value='Weekly'></input> Weekly"
+				+ "<input type='radio' name='order' value='Daily'></input> Daily"
 				+ "<input type='radio' name='order' value='Monthly'></input> Monthly"
 				+ "<input type='radio' name='order' value='Yearly'></input> Yearly<br><br>"
 				+ "<input type='submit' name='analyze' value='Analyze'>"
@@ -48,9 +47,10 @@ public class Olap extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// creating parameters
-		String owner = request.getParameter("imageowner");
+		String ownerBox = request.getParameter("imageowner");
 		String subjectBox = request.getParameter("subjectBox");
 		String subject = request.getParameter("subjectText");
+                String owner = request.getParameter("ownerText");
 		String timeBox = request.getParameter("time");
 		String fromTime = request.getParameter("fromTime");
 		String toTime = request.getParameter("toTime");
@@ -70,16 +70,22 @@ public class Olap extends HttpServlet {
 			out.println("<p>All fields were left blank");
 		} else {
 			// if owner box is ticked, data cube for owner
-			if (owner != null && owner.equals("1")) {
+			if (ownerBox != null && ownerBox.equals("1")) {
 				if (element > 0) {
 					select += ", ";
 					groupby += ", ";
 				}
 				select = select + "owner_name";
 				groupby = groupby + "owner_name";
-
+                                if (!owner.isEmpty()){
+                                  where +="(owner_name like '%" + owner+ "%') ";
+                                }
 				view += "<th><b> Owner </b></th>";
 				element++;
+                                if ((timeBox != null && timeBox.equals("1"))
+                                    ||(subjectBox!=null && subjectBox.equals("1"))) {
+					where += " and ";
+				}
 			}
 			// if subject box is ticked, datacube for owner. If keyword in
 			// subject, use subject to search
@@ -136,17 +142,17 @@ public class Olap extends HttpServlet {
 				select += ", ";
 				endGroupBy += ", ";
 			}
-			if (orderOp.equals("Weekly")) {
-				header = "week of";
-				select += "extract(day from trunc(timing,'IW'))";
-				endGroupBy += "extract(day from trunc(timing,'IW'))";
+			if (orderOp.equals("Daily")) {
+				header = "Day";
+				select += "extract(day from timing)";
+				endGroupBy += "extract(day from timing)";
 			}
 			if (orderOp.equals("Monthly")) {
 				header = "Month";
 				select += "extract(month from timing)";
 				endGroupBy += "extract(month from timing)";
 			}
-			if (orderOp.equals("Year")) {
+			if (orderOp.equals("Yearly")) {
 				header = "Year";
 				select += "extract(year from timing)";
 				endGroupBy += "extract(year from timing)";
@@ -183,7 +189,7 @@ public class Olap extends HttpServlet {
 		}
 		view = view + "<th><b>Number of Images</b></th>";
 
-		out.println(sql);
+		//out.println(sql);
 		out.println("<hr>");
 		out.println("<table border = '1'><tr>" + view + "</tr>");
 		db newDB = new db();
